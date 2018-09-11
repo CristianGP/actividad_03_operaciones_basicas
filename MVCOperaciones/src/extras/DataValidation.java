e030ce8d3342255b960f0379a6cdc553a683b997
Cristian Gayosso Peralta <1716110078@utec-tgo.edu.mx>
2018-09-11 13:45:51 -0500

    Calculadora

---
+++ MVCOperaciones/src/extras/DataValidation.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package extras;
+
+import javax.swing.JOptionPane;
+
+public class DataValidation {
+   public int string2Int(String value){
+       int result = 0;
+       try {
+           result = Integer.parseInt(value);
+       
+       } catch (NumberFormatException err) {
+           JOptionPane.showMessageDialog(null, "Error al convertir el valor");
+           result = 0;
+       }
+       return  result;
+   }
+}
